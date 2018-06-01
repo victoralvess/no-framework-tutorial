562de2fbf5358cc8129073abe69d98313808d3ca
Victor Alves <vsilva.programador@gmail.com>
2018-06-01 11:45:21 -0300

    add dynamic content controller

--- src/Controllers/Page.php
+++ src/Controllers/Page.php
@@ -4,11 +4,41 @@ declare(strict_types = 1);
 
 namespace Example\Controllers;
 
+use Http\Response;
+use Example\Template\Renderer;
+use Example\Page\PageReader;
+use Example\Page\InvalidPageException;
+
 class Page
 {
+    private $response;
+    private $renderer;
+    private $pageReader;
+
+    public function __construct(
+        Response $response,
+        Renderer $renderer,
+        PageReader $pageReader
+    ) {
+        $this->response = $response;
+        $this->renderer = $renderer;
+        $this->pageReader = $pageReader;
+    } 
+
     public function show($params): void
     {
-        var_dump($params);
+      $slug = $params['slug'];
+      try {
+          $data['content'] = $this->pageReader->readBySlug($slug);
+      } catch (InvalidPageException $e) {
+          $this->response->setStatusCode(404);
+          $this->response->setContent('404 - Page not found');
+
+          return;
+      }
+
+        $html = $this->renderer->render('Page', $data);
+        $this->response->setContent($html);
     }
 }
 
